53be0746d23acbd2b280cbfe86293683f2f96e24
kold <alswjd4047@naver.com>
2022-11-27 17:32:12 +0900

    [pathway1] pathway1 완성
    - Icon, stringResource, Card 학습

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -4,21 +4,27 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
+
+data class MainItem(val name: String, val content: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,46 +55,71 @@ private fun KoldApp(modifier: Modifier = Modifier) {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    names: List<MainItem> = List(1000) {
+        MainItem(
+            "$it",
+            stringResource(id = R.string.composem_ipsum).repeat(3)
+        )
+    }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { item ->
-            Greeting(name = item)
+            Greeting(name = item.name, contents = item.content)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+fun Greeting(
+    name: String,
+    contents: String
+) {
+    Card(
+        colors = CardDefaults.cardColors(),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // 주의: coerceAtLeast함수가 없으면 spring 애니메이션 적용 시에 padding must non-negative 오류가 발생함.
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium)
+        CardContent(name, contents)
+    }
+}
+
+@Composable
+private fun CardContent(name: String, contents: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium)
+            if (expanded) {
+                Text(text = contents, fontSize = 10.sp, lineHeight = 15.sp)
             }
-            ElevatedButton(
-                onClick = {
-                    expanded = !expanded
-                }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
+        }
+        IconButton(
+            onClick = {
+                expanded = !expanded
             }
+        ) {
+            val iconSet = if (expanded) {
+                Icons.Filled.ExpandLess to stringResource(id = R.string.show_less)
+            } else {
+                Icons.Filled.ExpandMore to stringResource(id = R.string.show_more)
+            }
+
+            Icon(
+                imageVector = iconSet.first,
+                contentDescription = iconSet.second
+            )
         }
     }
 }
